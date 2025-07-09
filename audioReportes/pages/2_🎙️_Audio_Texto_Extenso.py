6895835774fce50ea69ec6de28bf78e505c07227
Pablo David Minango Negrete <42147807+pdavicho@users.noreply.github.com>
2025-07-09 16:07:05 -0500

    Update 2_🎙️_Audio_Texto_Extenso.py
    Esto hace que cualquier llamada a ffmpeg desde Whisper o Pydub funcione sin errores, incluso en entornos donde el binario no está globalmente instalado (como Streamlit Cloud).

--- audioReportes/pages/2_🎙️_Audio_Texto_Extenso.py
+++ audioReportes/pages/2_🎙️_Audio_Texto_Extenso.py
@@ -24,6 +24,12 @@ from reportlab.platypus import Image
 from datetime import datetime
 import io
 
+import imageio_ffmpeg
+
+# Añadir la ruta del ejecutable ffmpeg al PATH
+ffmpeg_path = imageio_ffmpeg.get_ffmpeg_exe()
+os.environ["PATH"] += os.pathsep + os.path.dirname(ffmpeg_path)
+
 st.set_page_config(page_title='Audio Texto Extenso', page_icon=':studio_microphone:', layout="wide")
 
 # Inicializar session state
